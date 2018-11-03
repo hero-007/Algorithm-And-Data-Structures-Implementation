a06bdbe1865fe46471cf181a80ec7ade810a3971
hero-007 <akhil1998tiwari@gmail.com>
2018-11-03 12:58:38 +0530

    Implemented stack using stl in c++

---
+++ Data Structures/stack-stl.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	stack<int> mystack;
+	
+	mystack.push(0);
+	mystack.push(1);
+	mystack.push(2);
+	
+	// Printing the content of the stack and there respective index values
+	
+	while(mystack.empty() == false)
+	{
+		printf("%d \n",mystack.top());
+    mystack.pop();
+		}	
+	
+	return 0;
+}
